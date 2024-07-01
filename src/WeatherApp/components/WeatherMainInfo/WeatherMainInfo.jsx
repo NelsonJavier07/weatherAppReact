724b4ee988e8c9dc39e19044df91441a26b652f8
Nelson <nelson070585@gmail.com>
2024-06-30 22:35:29 -0300

    correccion de mapa, ademas de instalacion de tecnologia tailwindcss para estilos

--- src/WeatherApp/components/WeatherMainInfo/WeatherMainInfo.jsx
+++ src/WeatherApp/components/WeatherMainInfo/WeatherMainInfo.jsx
@@ -1,5 +1,7 @@
 export const WeatherMainInfo = ({weather}) =>{
 
+    console.log(weather?.location.lon);
+    console.log(weather?.location.lat);
 
     return(
         <div>
@@ -20,10 +22,10 @@ export const WeatherMainInfo = ({weather}) =>{
             <div>
                 <iframe
                     title="mapa"
-                    src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d317716.60648832726!2d${weather?.location.lon}01!3d${weather?.location.lat}4!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondres%2C%20Reino%20Unido!5e0!3m2!1ses-419!2sar!4v1719709298074!5m2!1ses-419!2sar`}
+                    src={`https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13897.215629119919!2d${weather?.location.lon}!3d${weather?.location.lat}2!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sar!4v1719796088051!5m2!1ses-419!2sar`}
                     width="600" 
                     height="450" 
-                    style={{ border:1 }} 
+                    style={{ border:0 }} 
                     loading="lazy" 
                     referrerPolicy="no-referrer-when-downgrade"
                 ></iframe>
@@ -33,3 +35,6 @@ export const WeatherMainInfo = ({weather}) =>{
 }
 
 //<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d317716.60648832726!2d-0.4312474072555601!3d51.528607007953084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondres%2C%20Reino%20Unido!5e0!3m2!1ses-419!2sar!4v1719709298074!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+//<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d317716.60648832715!2d-0.43124740725554217!3d51.5286070079531!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondres%2C%20Reino%20Unido!5e0!3m2!1ses-419!2sar!4v1719783513207!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+//<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13897.215629119919!2d-66.86775635!3d-29.449134799999992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sar!4v1719796088051!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
